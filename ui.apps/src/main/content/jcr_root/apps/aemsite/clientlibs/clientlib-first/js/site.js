4d335c4b4821f94ba8e6b8edb13aa98f588ccb67
AemStudentNumber4 <aemstudentnumber4@gmail.com>
2023-10-22 22:39:48 +0200

    feat: solve js task

--- ui.apps/src/main/content/jcr_root/apps/aemsite/clientlibs/clientlib-first/js/site.js
+++ ui.apps/src/main/content/jcr_root/apps/aemsite/clientlibs/clientlib-first/js/site.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Client library loaded successfully!");
+document.addEventListener("DOMContentLoaded", function() {
+  myFunction();
 });
+
+function myFunction() {
+
+  var textContainer = document.querySelector(".text-container");
+  console.log(textContainer);
+  var shouldModify = textContainer.dataset.shouldModify;
+  console.log(shouldModify);
+
+  if(shouldModify) {
+    console.log('Modify');
+    var paragraph = textContainer.querySelector("p");
+    console.log(paragraph);
+    var text = paragraph.textContent;
+    console.log(text);
+    var updatedText = text.replace(/positive/g, 'Alladin');
+    console.log(updatedText);
+    paragraph.textContent = updatedText;
+  }
+}
