0423564fac6177c014ef4c938c97cf487d2fea30
AemStudentNumber4 <aemstudentnumber4@gmail.com>
2023-10-28 13:02:49 +0200

    feat: add the Sling model to represent demoResource

---
+++ core/src/main/java/com/aemsite/core/models/DemoResource.java
@@ -0,0 +1,18 @@
+package com.aemsite.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class)
+public class DemoResource {
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Default(values = "No message")
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+}
